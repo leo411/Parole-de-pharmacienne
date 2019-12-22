13655e4f11c771ed32b9966a10105e681c9c9efb
Leopoldine Hallynck <lhallynck@gmail.com>
2019-12-22 21:43:15 +0100

    chgmt favicon, faire un composant de bloglist, ajout fu footer avec le coeur, chgmt de la bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,44 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Button from "../components/button"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Button from '../components/button'
+
+import rabbit from '../images/lapin_04.jpg'
 
 class IndexPage extends React.Component {
-  render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    render() {
+        const siteTitle = 'Le blog de Léopoldine'
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>This starter comes out of the lify CMS.</p>
-        <p>Now go build something great! prout</p>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
-      </Layout>
-    )
-  }
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <SEO
+                    title="Home"
+                    keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+                />
+                <img src={rabbit} alt="" />
+                <h1>
+                    Hey people{' '}
+                    <span role="img" aria-label="wave emoji">
+                        👋
+                    </span>
+                </h1>
+                <p>
+                    Bienvenue sur le blog de Léopoldine. Nous allons parler de
+                    lapins. Vous êtes actuellement sur la page d'accueil.
+                </p>
+                <p>
+                    Pour en découvrir plus sur les lapins je vous invite à
+                    visiter mon blog.
+                </p>
+                <p>Bonne lecture!</p>
+                <Link to="/blog/">
+                    <Button marginTop="35px">Go to Blog</Button>
+                </Link>
+            </Layout>
+        )
+    }
 }
 
 export default IndexPage
