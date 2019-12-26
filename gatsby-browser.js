7acf1b4c101fe84a5b94844844f468a2fb98a150
Leopoldine Hallynck <lhallynck@gmail.com>
2019-12-26 20:29:08 +0100

    add bootstrap and jquery

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 // custom typefaces
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './src/styles/global.css'
